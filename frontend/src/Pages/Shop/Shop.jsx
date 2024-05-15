9f9852bf026a53a65e6c36355cab7e87d737e79b
Madhavan R <rmadhavan112000@gmail.com>
2024-05-15 06:28:43 +0530

    Added backend URL

--- frontend/src/Pages/Shop/Shop.jsx
+++ frontend/src/Pages/Shop/Shop.jsx
@@ -16,7 +16,7 @@ const Shop = () => {
     useEffect(() => {
         const fetchProducts = async () => {
             try{
-                const response = await fetch('http://localhost:3001/products');
+                const response = await fetch('https://aravind-opticals.onrender.com/products');
                 if(!response.ok){
                     throw new Error('Failed to fetch products');
                 }
